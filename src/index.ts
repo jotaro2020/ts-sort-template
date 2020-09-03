3ae618e74817d48dd590b58fd4fb6df092e85fd5
Zhassulan Abishev <zhaslan27abishev@gmail.com>
2020-09-03 11:21:41 +0600

    added LinkedList and some Abstract class

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
-import {Sorter} from "./Sorter";
 import {NumbersCollection} from "./NumbersCollection";
 import {CharactersCollection} from "./CharactersCollection";
+import {LinkedList} from "./LinkedList";
 
 const numberCollection=new NumbersCollection([1,24,0,3,-1,30])
+numberCollection.sort()
+console.log(numberCollection)
+
+
 const charactersCollection=new CharactersCollection("Xasdas");
-const sorter=new Sorter(charactersCollection);
-sorter.sort()
+charactersCollection.sort();
 console.log(charactersCollection);
+
+const linkedList=new LinkedList();
+linkedList.add(540);
+linkedList.add(40);
+linkedList.add(50);
+linkedList.add(-2);
+linkedList.add(20);
+
+linkedList.sort()
+linkedList.print()
